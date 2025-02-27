efe4323a2be85dabe22c2cca2f077ae14c0ebd0e
StevenBS16 <orodriguez1497@gmail.com>
2025-02-27 18:13:39 +0000

    final version refactorizado

--- src/Kata.java
+++ src/Kata.java
@@ -2,26 +2,22 @@ public class Kata {
 
     public static int sum(int[] numbers)
     {
-        int numMin = numbers[0], numMax = numbers[0], sum = 0;
-        if (numbers.length == 1){
-            return 0;
-        }else{
-            for(int i = 0; i < numbers.length; i++){
-                if (numbers[i] >= numMax ){
-                    numMax = numbers[i];
-                    sum += numbers[i];
-                }else if (numbers[i] <= numMin){
-                    numMin = numbers[i];
-                    sum += numbers[i];
-                }else if(numbers[i] > numMin && numbers[i] < numMax){
+        if(numbers !=null && numbers.length!=0){
+            int numMin = numbers[0], numMax = numbers[0], sum = 0;
+            if (numbers.length == 1){
+                return 0;
+            }else{
+                for(int i = 0; i < numbers.length; i++){
+                    if (numbers[i] >= numMax ){
+                        numMax = numbers[i];
+                    }else if (numbers[i] <= numMin){
+                        numMin = numbers[i];
+                    }
                     sum += numbers[i];
                 }
             }
+            return (sum - numMin) - numMax;
         }
-        return (sum - numMin) - numMax;
+        return 0;
     }
 }
-/*En la kata del codewars hay un error que no se solucionar, los test
-de los arrays nulos y los vacios, el resto de tets salen correctos
- */
-
