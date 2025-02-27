b9520623d8a2a58dabc061d03014fc5e28d9f357
Omar <orodriguez1497@gmail.com>
2025-02-27 00:38:24 +0000

    Corrección del código para superar los testeos

--- src/Kata.java
+++ src/Kata.java
@@ -1,14 +1,27 @@
-public class Kata
-
-{
+public class Kata {
 
     public static int sum(int[] numbers)
-
     {
-
-        return 0;
-
+        int numMin = numbers[0], numMax = numbers[0], sum = 0;
+        if (numbers.length == 1){
+            return 0;
+        }else{
+            for(int i = 0; i < numbers.length; i++){
+                if (numbers[i] >= numMax ){
+                    numMax = numbers[i];
+                    sum += numbers[i];
+                }else if (numbers[i] <= numMin){
+                    numMin = numbers[i];
+                    sum += numbers[i];
+                }else if(numbers[i] > numMin && numbers[i] < numMax){
+                    sum += numbers[i];
+                }
+            }
+        }
+        return (sum - numMin) - numMax;
     }
-
 }
+/*En la kata del codewars hay un error que no se solucionar, los test
+de los arrays nulos y los vacios, el resto de tets salen correctos
+ */
 
--- src/KataTest.java
+++ src/KataTest.java
@@ -1,11 +1,14 @@
-import static org.junit.jupiter.api.Assertions.*;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
-class KataTest {
+public class KataTest {
     @Test
 
     public void BasicTests() {
 
         assertEquals(16, Kata.sum(new int[] { 6, 2, 1, 8, 10}));
+        assertEquals(-5, Kata.sum(new int[] { 6, -2, 1, 8, -10, -10}));
+        assertEquals(0, Kata.sum(new int[] {10}));
 
     }
 
